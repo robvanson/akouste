52f5834029bec23775eacd25b8affabd47c55c4f
Rob van Son <r.j.j.h.vanson@gmail.com>
2019-10-03 16:18:12 +0200

    Added beforeunloaded and multiple ranges

--- InitSliderangeListeningExperiment.js
+++ InitSliderangeListeningExperiment.js
@@ -1,8 +1,12 @@
 // Shuffle stimuli or not
 ShuffleStimulusList = true;
+
 // If practice items > 0, the last PracticeItems stimuli will be 
 // prepended in reverse order. (NOT YET IMPLEMENTED)
 PracticeItems = 4;
+
+// Set number of visible sliders
+var NumberOfRanges = 1;
 
 // Set language
 var language = "NL";
@@ -13,8 +17,15 @@ var IntroductionText = [];
 var StimulusNumberText = [];
 var PlayAText = [];
 var NextText = [];
-var LeftText = [];
-var RightText = [];
+var HeaderLineText0 = [];
+var RightText0 = [];
+var LeftText0 = [];
+var HeaderLineText1 = [];
+var RightText1 = [];
+var LeftText1 = [];
+var HeaderLineText2 = [];
+var RightText2 = [];
+var LeftText2 = [];
 var ReadyText = [];
 var SaveText = [];
 var SaveLinkText = [];
@@ -29,8 +40,15 @@ IntroductionText ["EN"] = "The speech has been manipulated. How distorted does i
 StimulusNumberText ["EN"] = "XXX answers to go";
 PlayAText ["EN"] = " Speech &#9658; ";
 NextText ["EN"] = "Next &rarr;";
-LeftText ["EN"] = "Not at all<br />distorted";
-RightText ["EN"] = "Very<br />distorted";
+HeaderLineText0 ["EN"] = "The speech sounds distorted";
+RightText0 ["EN"] = "Not at all";
+LeftText0 ["EN"] = "Very";
+HeaderLineText1 ["EN"] = "The speech sounds distorted";
+RightText1 ["EN"] = "Not at all";
+LeftText1 ["EN"] = "Very";
+HeaderLineText2 ["EN"] = "The speech sounds distorted";
+RightText2 ["EN"] = "Not at all";
+LeftText2 ["EN"] = "Very";
 ReadyText ["EN"] = "Ready";
 SaveLinkText ["EN"] = 'Save Results';
 RestartPageText ["EN"] = 'Restart';
@@ -44,8 +62,15 @@ IntroductionText ["NL"] = "De spraak is gemanipuleerd. Hoe vervormd klinkt het?"
 StimulusNumberText ["NL"] = "XXX antwoorden te gaan";
 PlayAText ["NL"] = " Spraak &#9658; ";
 NextText ["NL"] = "Volgende &rarr;";
-LeftText ["NL"] = "Helemaal niet<br />vervormd";
-RightText ["NL"] = "Heel erg<br \>vervormd";
+HeaderLineText0 ["NL"] = "De spraak klinkt vervormd";
+RightText0 ["NL"] = "Helemaal niet";
+LeftText0 ["NL"] = "Heel erg";
+HeaderLineText1 ["NL"] = "De spraak klinkt vervormd";
+RightText1 ["NL"] = "Helemaal niet";
+LeftText1 ["NL"] = "Heel erg";
+HeaderLineText2 ["NL"] = "De spraak klinkt vervormd";
+RightText2 ["NL"] = "Helemaal niet";
+LeftText2 ["NL"] = "Heel erg";
 ReadyText ["NL"] = "Klaar";
 SaveLinkText ["NL"] = 'Bewaar resultaten';
 RestartPageText ["NL"] = 'Opnieuw';
@@ -60,7 +85,25 @@ function replaceTexts (language) {
 	document.getElementById('StimulusNumberText').innerHTML = StimulusNumberText[language];
 	document.getElementById('PlayAText').innerHTML = PlayAText[language];
 	document.getElementById('NextText').innerHTML = NextText[language];
-	document.getElementById('LeftText').innerHTML = LeftText[language];
-	document.getElementById('RightText').innerHTML = RightText[language];
+	document.getElementById('HeaderLineText0').innerHTML = HeaderLineText0[language];
+	document.getElementById('RightText0').innerHTML = RightText0[language];
+	document.getElementById('LeftText0').innerHTML = LeftText0[language];
+	document.getElementById('HeaderLineText1').innerHTML = HeaderLineText1[language];
+	document.getElementById('RightText1').innerHTML = RightText1[language];
+	document.getElementById('LeftText1').innerHTML = LeftText1[language];
+	document.getElementById('HeaderLineText2').innerHTML = HeaderLineText1[language];
+	document.getElementById('RightText2').innerHTML = RightText2[language];
+	document.getElementById('LeftText2').innerHTML = LeftText2[language];
 	document.getElementById('RestartPageText').innerHTML = RestartPageText[language];
+
+	// Make the required number of sliders visible
+	for(var i = 1; i < NumberOfRanges; i++) {
+		document.getElementById('EmptyRow'+i).style.display = '';
+		document.getElementById('HeaderLineRow'+i).style.display = '';
+		document.getElementById('RangeRow'+i).style.display = '';
+		document.getElementById('HeaderLineText'+i).style.visibility = 'visible';
+		document.getElementById('RightText'+i).style.visibility = 'visible';
+		document.getElementById('LeftText'+i).style.visibility = 'visible';
+		document.getElementById('Sliderange'+i).style.visibility = 'visible';
+	};
 };
