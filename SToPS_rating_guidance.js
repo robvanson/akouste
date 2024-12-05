04670ae81f340c0fccfd53b36225019bd1772e86
Rob van Son <r.j.j.h.vanson@gmail.com>
2024-12-05 10:24:32 +0100

    Added STOPS questionnaire example

---
+++ SToPS_rating_guidance.js
@@ -0,0 +1,179 @@
+var SToPS_guidance_Q1 = `
+1. Overall Voice Rating
+
+Voice quality is not compared to normal voice for a laryngeal speaker. Rate the voice in comparison to your internal reference point of voice potential for surgical voice restoration (SVR) speakers. 
+0. Excellent - The best voice achievable for a SVR speaker; the voice quality you would judge to be the optimal outcome after laryngectomy. 
+1. Good - Some aspect(s) observed prevents you judging the voice as falling into the optimal outcome group. 
+2. Adequate - Some aspect(s) mean the voice cannot be rated as good 
+3. Poor - The worst outcome for a SVR speaker. 
+`
+
+var SToPS_guidance_Q2 = `
+2. Perceptual Voice Tonicity
+
+Tonic 
+0. Neutral tone; neither lax nor tight. 
+
+Hypotonic (tone laxer than tonic) - example provided
+1. Mildly laxer compared to tonic (Lee Marvin voice, like creak) 
+2. Moderately lax compared to tonic; voice may have ‘echoing’ sound of resonance of voice in the inflated hypotonic area. Creaky, lax feature and low pitch. 
+3. Severe hypotonicity for laryngectomy, but would be classed as good outcome for a jejunum or stomach graft. Obvious echoing resonance. Whisper quality is evident in the lax, inflated area. Low pitched. 
+4. Usually only jejunum/stomach pull-up patients display this degree of hypotonicity. The voice is severely whispery and has reduced volume compared to hypotonic 3. Echoing Resonance in the ballooning, inflated hypotonic area is severe. 
+5. Aphonic whisper. This differs from the aphonia in a stenosed neopharynx as air is passing through larger, laxer, ballooning area with less turbulence than a tight stenosed area. Tight stenosed voice sounds more like tense aphonia in a patient with a larynx. The volume is severely reduced. Intermittent gurgly phonation may occur due to vibration of secretions. 
+
+Hypertonic (tone tighter than tonic) - example provided
+1. Mildly tenser than tonic. Quality sounds more like a dysphonic voice (in patient with a larynx). No strain. 
+2. Moderately tenser than tonic, but not to the degree that would be considered sufficient for botulinum toxin. Strain is evident but only mild. Volume may be reduced or louder than normal. No major effect on fluency. 
+3. Definitely hypertonic, moderately strained or whisper quality. Mild effect on fluency. 
+4. Marked hypertonic quality that is unpleasant to listen to. Voice is still functional but with marked strain and markedly reduced fluency. 
+5. Severe hypertonicity, fluency is severely affected and intermittent total spasm may occur. The voice is normally non-functional or cannot be used for all communication needs due to the strain required for phonation. 
+
+Stenosis 
+Stenosis is not rated 1-5; it could only be rated as a separate parameter from tonicity in its most marked form in the pilot study. Stenosis +5 should be used if no tonicity is judged to be present due to extensive neopharyngeal fibrosis. Marked stenosis causes a rigid, immobile neopharynx. Stenosis (+5) is characterized as an aphonic whisper that gives the impression of a scarred, tight neopharynx with resonance of the whisper in a rigid tube with no vibrating neoglottis. Strain may be a feature if the diameter of the rigid area is narrow. The voice often sounds similar to that of a laryngeal speaker with aphonia; N.B. hypotonic -5 has a lower resonance and is a lax aphonia. Stenotic voice quality is always associated with dysphagia for solids.
+`
+
+var SToPS_guidance_Q2A = `
+2. Perceptual Voice Tonicity
+
+Tonic 
+0. Neutral tone; neither lax nor tight. 
+
+Hypotonic (tone laxer than tonic) - example provided
+1. Mildly laxer compared to tonic (Lee Marvin voice, like creak) 
+2. Moderately lax compared to tonic; voice may have ‘echoing’ sound of resonance of voice in the inflated hypotonic area. Creaky, lax feature and low pitch. 
+3. Severe hypotonicity for laryngectomy, but would be classed as good outcome for a jejunum or stomach graft. Obvious echoing resonance. Whisper quality is evident in the lax, inflated area. Low pitched. 
+4. Usually only jejunum/stomach pull-up patients display this degree of hypotonicity. The voice is severely whispery and has reduced volume compared to hypotonic 3. Echoing Resonance in the ballooning, inflated hypotonic area is severe. 
+5. Aphonic whisper. This differs from the aphonia in a stenosed neopharynx as air is passing through larger, laxer, ballooning area with less turbulence than a tight stenosed area. Tight stenosed voice sounds more like tense aphonia in a patient with a larynx. The volume is severely reduced. Intermittent gurgly phonation may occur due to vibration of secretions. 
+`
+
+var SToPS_guidance_Q2B = `
+2. Perceptual Voice Tonicity
+
+Tonic 
+0. Neutral tone; neither lax nor tight. 
+
+Hypertonic (tone tighter than tonic) - example provided
+1. Mildly tenser than tonic. Quality sounds more like a dysphonic voice (in patient with a larynx). No strain. 
+2. Moderately tenser than tonic, but not to the degree that would be considered sufficient for botulinum toxin. Strain is evident but only mild. Volume may be reduced or louder than normal. No major effect on fluency. 
+3. Definitely hypertonic, moderately strained or whisper quality. Mild effect on fluency. 
+4. Marked hypertonic quality that is unpleasant to listen to. Voice is still functional but with marked strain and markedly reduced fluency. 
+5. Severe hypertonicity, fluency is severely affected and intermittent total spasm may occur. The voice is normally non-functional or cannot be used for all communication needs due to the strain required for phonation. 
+
+Stenosis 
+Stenosis is not rated 1-5; it could only be rated as a separate parameter from tonicity in its most marked form in the pilot study. Stenosis +5 should be used if no tonicity is judged to be present due to extensive neopharyngeal fibrosis. Marked stenosis causes a rigid, immobile neopharynx. Stenosis (+5) is characterized as an aphonic whisper that gives the impression of a scarred, tight neopharynx with resonance of the whisper in a rigid tube with no vibrating neoglottis. Strain may be a feature if the diameter of the rigid area is narrow. The voice often sounds similar to that of a laryngeal speaker with aphonia; N.B. hypotonic -5 has a lower resonance and is a lax aphonia. Stenotic voice quality is always associated with dysphagia for solids.
+`
+
+var SToPS_guidance_Q3 = `
+3. Strain (Audible Effort for Voicing)
+
+The amount of audible effort you perceive the patient requires to produce voice. 
+0. No perceived effort. 
+1. Mild 
+2. Moderate 
+3. Severe, usually associated with marked hypo/hypertonicity. 
+`
+
+var SToPS_guidance_Q4 = `
+4. Wetness (gurgliness) of Voice Quality
+
+The perceptual feature of secretions bubbling in the neopharynx on voicing. If an intermittent feature, rate at its most severe. 
+0. No audible vibration of secretions 
+1. Mild 
+2. Moderate 
+3. Severe - usually associated with jejunal grafts and hypotonicity +3 to +5. May occur with dysphagia if pooling of secretions or liquid bolus in stenosis or pouch/pseudoepiglottis. 
+`
+
+var SToPS_guidance_Q5 = `
+5. Impairment of volume
+
+0. Conversational volume of voice judged to be within the same limits as expected for normal conversational volume for a laryngeal speaker. 
+1. Mildly impaired volume 
+2. Moderately impaired volume 
+3. Severely impaired volume reserved for voice that is whisper only Aphonia +5/-5/Stenosis. 
+`
+
+var SToPS_guidance_Q6 = `
+6. Impairment of Social Acceptability of Voice
+
+If you are judging social acceptability to be impaired because of regional accent, please make a note of this. 
+0. Social acceptability is the optimal level possible for a SVR speaker. 
+1. Mild impairment , e.g. mildly gurgly quality, strain etc 
+2. Moderate impairment; obviously qualitatively different to a laryngeal speaker and not aesthetically pleasant. 
+3. Severe impairment of acceptability. “General public” would tend to turn or stare if they heard this voice e.g. marked stoma blast, echoing deep jejunal voice, severe hypertonic strain. The type of voice outcome you would dread if this subject were your relative. This parameter has the potential to link with one or more of the other parameters on the scale. 
+`
+
+var SToPS_guidance_Q7 = `
+7. Whisper
+
+The perceptual impression of whisperiness in the voice quality. 
+0. No whisper quality audible 
+1. Mild whisper quality 
+2. Moderate 
+3. Severe. Total aphonia. 
+`
+
+var SToPS_guidance_Q8 = `
+8. Impairment of Intelligibility
+
+0. Ease of understanding the speaker that would be expected for a normal laryngeal speaker, in a one to one speaking situation with no background noise. 
+1. Mild impairment of intelligibility 
+2. Moderate impairment of intelligibility 
+3. Severe impairment of intelligibility 
+`
+
+var SToPS_guidance_Q9 = `
+9. Stoma blast
+
+A stoma blast is a high frequency noise generated from the tracheostoma - example provided
+0. Stoma noise is judged to be absent 
+1. Intermittent mild stoma noise; rate in this category even if a brief instance of mild stoma noise is audible in the sample 
+2. Constant stoma noise even if you judge it as being relatively quiet or mild. 
+3. Constantly audible stoma noise that is marked and may compete with oral speech. 
+`
+
+var SToPS_guidance_Q10 = `
+10. Impairment of Fluency
+
+0. Fluency within normal limits for a typical laryngeal speaker. 
+1. Mildly impaired fluency compared to a typical laryngeal speaker. 
+2. Moderate impaired fluency - 5 – 10 syllable phrasing per breath group 
+3. Severely impaired fluency - phrasing of 5 syllables or less. 
+`
+
+var SToPS_guidance_Q11 = `
+11. Impairment of Articulatory Precision
+
+0. “Average” precision of articulation as defined as a score of 1 or 2 out of 6 in the Vocal Profile Analysis. 
+1. Habitually lax articulation (score of 3 or 4 on the VPA). 
+2. Markedly habitual lax articulation (VPA 5 or 6) or mild dysarthria. A naïve listener would describe this as “mumbling” due to lack of precision or “slurring” due to mild dysarthria. 
+3. Moderate - severe dysarthria or a very severe premorbid articulation disorder; there would be marked intelligibility issues even if the subject were still a laryngeal speaker. 
+`
+
+var SToPS_guidance_Q12 = `
+12. Positive features of articulation
+
+Certain speakers have speech diction, intonation, and/or pause features that have an overall positive effect but are not part of the voice signal. These have the potential to positively affect the judgement of naive listerners.
+0. No specific positive features are judged to be present.
+1. Positive features that are above average in comparison to laryngectomy peers; prosody (intonation) judged as present.
+2. Excellent phrasing, diction, and intonation.
+3. Outstanding features such as noted in newsreaders, with ormal or almost normal intonation present.
+`
+
+var SToPS_guidance_Q13 = `
+13. Accent
+
+0. No discernible regional accent. 
+1. Mild accent. 
+2. Moderate accent. 
+3. Marked accent. 
+`
+
+var SToPS_guidance_Q14 = `
+14. Poor reader
+
+0. No problems with reading aloud. 
+1. Mild problem with reading aloud. 
+2. Moderate problem. 
+3. Severe problem. 
+`
+
