b782e52b68eaa15ab489cc79d1b6586b348d1f30
Rob van Son <R.J.J.H.vanSon@gmail.com>
2025-05-08 17:17:12 +0200

    Added ABXexample markdown

---
+++ ABXexample_markdown.js
@@ -0,0 +1,62 @@
+initialMarkdown = ` 
+<center>
+
+# Who is the unknown speaker? Speaker A or B?
+
+## The voices of the speakers A and B have been changed. Which one do you think is the unknown speaker X?
+
+[//comment]: // "Do not change the items between <>-brackets, unless you know what you are doing."
+[//comment]: // "You can change the '0 answers to go' text, but leave a 0 (or any number)"
+<h2>
+<div id="StimulusNumberText">0 answers to go</div>
+</h2>
+
+[//comment]: // "End of No Change"
+
+|     |                     |   |                      |   |                     |    |
+| -: | --------------: | - | :-------------: | - | :------------- | :- |
+|     | [[[A ►|A]]] |   | [[[X ►|X]]] |   | [[[B ►|B]]] |    |
+|     |                      |   |                     |   |                     |    | 
+ 
+</center>
+
+------------------------------------------------------
+
+<center>
+
+### Which spreaker, A or B is the unknown speaker X
+|    |    |              |                         |                |    |   |
+| - | -: | :-------: | :----------------: | :---------: | :- | - |
+|    |   |     (())    |  Your Choice   |      (())      |   |   |
+|    |   |  **A**  |                         |   **B**   |    |   |
+
+
+</center>
+
+[//comment]: # "These are internal parameters for the experiment and visible texts not in this Markdown"
+[//comment]: # "----------"
+[//parameter]: # "ExperimentAcronym:ABXexample"
+[//parameter]: # "audioBaseURL:./Stimuli/"
+[//parameter]: # "PracticeItems:4"
+[//parameter]: # "ShuffleStimuli:true"
+[//parameter]: # "RandomizeAB:false"
+[//buttontext]: # "NextText:Next"
+[//buttontext]: # "NextAlertText:Please listen to the recordings and answer the questions first"
+[//buttontext]: # "ReadyText:Ready"
+[//buttontext]: # "PlayText: "
+[//buttontext]: # "RestartPageText:Restart"
+[//buttontext]: # "SaveButtonText:Save Results"
+[//buttontext]: # "SaveText:Please click XXSaveButtonTextXX and store the file"
+[//tooltiptext]: # "ToolTipPlay:Play sound"
+[//tooltiptext]: # "ToolTipNext:Go to next item"
+[//tooltiptext]: # "ToolTipReadyReady please save results"
+[//tooltiptext]: # "ToolTipRestart:Start a new experiment session"
+[//tooltiptext]: # "ToolTipSave:Save the answer to a file"
+[//comment]: # "----------"
+[//comment]: # "These are stimuli for this experiment"
+[//comment]: # "----------"
+[//stimulus0]: # "A,B,X,LangA,LangB,LangX"
+[//stimulus1]: # "https://upload.wikimedia.org/wikipedia/commons/6/62/De-Aluminium.ogg,https://upload.wikimedia.org/wikipedia/commons/e/e7/Fr-bordure.ogg,https://upload.wikimedia.org/wikipedia/commons/d/db/En-uk-illustrate.ogg,De,Fr,En"
+[//stimulus1]: # "https://upload.wikimedia.org/wikipedia/commons/2/2d/Nl-aardhommel.ogg,https://upload.wikimedia.org/wikipedia/commons/8/8e/Or-ଅନୁଶୀଳନ.oga,https://upload.wikimedia.org/wikipedia/commons/d/da/L1188694-F1.oga,Nl,Or,Ar"
+[//comment]: # "----------"
+`
