10928a5283e6f17f56571882644522064a836f87
Rob van Son <R.J.J.H.vanSon@gmail.com>
2025-05-09 14:47:36 +0200

    Added JS version of Transcribe

---
+++ Transcribe_markdown.js
@@ -0,0 +1,74 @@
+initialMarkdown = ` 
+<center>
+
+# Transcribe the words
+
+### Transcribe the following word as good as you can
+
+[[[Word|A]]] 
+
+
+[//comment]: // "Do not change the items between <>-brackets, unless you know what you are doing."
+[//comment]: // "You can change the '0 answers to go' text, but leave a 0 (or any number)"
+<h2>
+<div id="StimulusNumberText">0 answers to go</div>
+</h2>
+
+[//comment]: // "End of No Change"
+
+</center>
+
+> You wil hear a word, likely not always in a language you will understand. Pleas transcribe the word as good as you can using your native writing system.
+
+<center>
+
+
+|                            |                                                                                                     |    |
+| -------------------: | :-------------------------------------------------------------------------: | :- | 
+| Enter your word: | <<|  transcribe {width:5cm;color:gray;text-align:center} |>>  |     |
+|                            |                                                                                                     |    |
+
+
+### Are you familiar with this language, {{{LangA}}} ?
+
+|    |     |              |          |     |     |
+| -: | -: | :--------: | :----: | :- | -: |
+|    |     |     (())    |   (())  |    |    |
+|    |     |     No    |   Yes  |    |    |
+
+</center>
+
+---------------------------
+
+[//comment]: # "These are internal parameters for the experiment and visible texts not in this Markdown"
+[//comment]: # "----------"
+[//parameter]: # "ExperimentAcronym:Transcribe"
+[//parameter]: # "audioBaseURL:./Stimuli/"
+[//parameter]: # "PracticeItems:4"
+[//parameter]: # "ShuffleStimuli:true"
+[//parameter]: # "RandomizeAB:false"
+[//buttontext]: # "NextText:Next"
+[//buttontext]: # "NextAlertText:Please listen to the recordings and answer the questions first"
+[//buttontext]: # "ReadyText:Ready"
+[//buttontext]: # "PlayText:Play"
+[//buttontext]: # "RestartPageText:Restart"
+[//buttontext]: # "SaveButtonText:Save Results"
+[//buttontext]: # "SaveText:Please click XXSaveButtonTextXX and store the file"
+[//tooltiptext]: # "ToolTipPlay:Play sound"
+[//tooltiptext]: # "ToolTipNext:Go to next item"
+[//tooltiptext]: # "ToolTipReadyReady please save results"
+[//tooltiptext]: # "ToolTipRestart:Start a new experiment session"
+[//tooltiptext]: # "ToolTipSave:Save the answer to a file"
+[//comment]: # "----------"
+[//comment]: # "These are stimuli for this experiment"
+[//comment]: # "----------"
+[//stimulus0]: # "A,LangA"
+[//stimulus1]: # "https://upload.wikimedia.org/wikipedia/commons/6/62/De-Aluminium.ogg,German (De)"
+[//stimulus1]: # "https://upload.wikimedia.org/wikipedia/commons/e/e7/Fr-bordure.ogg,French (Fr)"
+[//stimulus1]: # "https://upload.wikimedia.org/wikipedia/commons/d/db/En-uk-illustrate.ogg,English (En)"
+[//stimulus1]: # "https://upload.wikimedia.org/wikipedia/commons/2/2d/Nl-aardhommel.ogg,Dutch (Nl)"
+[//stimulus1]: # "https://upload.wikimedia.org/wikipedia/commons/8/8e/Or-ଅନୁଶୀଳନ.oga,Oriya (Or)"
+[//stimulus1]: # "https://upload.wikimedia.org/wikipedia/commons/d/da/L1188694-F1.oga,Arabic (Ar)"
+[//stimulus1]: # "https://upload.wikimedia.org/wikipedia/commons/2/2b/Ჟრუნი.oga,Georgian (Ka)"
+[//comment]: # "----------"
+`
