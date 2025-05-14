e19a7b396404e7f450ce7e5d0159da76db3b23a3
Rob van Son <R.J.J.H.vanSon@gmail.com>
2025-05-14 09:54:42 +0200

    Added Audiotest JS file

---
+++ Audiotest_markdown.js
@@ -0,0 +1,57 @@
+initialMarkdown = ` 
+<center>
+
+# akoúste test for local audio
+
+[[[A]]]
+
+
+## {{{!answerstogo!|font-weight:bold}}} answers to go
+
+Minimal test of audio. Uses two sounds from ./Stimuli as listed in Audiotest_stimuluslist.js (in the same folder as the wep page) or web audio when this stimuluslist is not available.
+
+For it to work, JavaScript should be allowed, *Third-party cookies* should be allowed (Chrome and derived browsers),  or *cross-site tracking* should be allowed (ie, *not* prevented, Safari). Firefox and derived browsers seem to work in most settings.
+
+</center>
+
+---------------------------
+
+<center>
+
+
+### Does this sound start with *ah*
+ |     |        |       |    | 
+ | -: | :--: | :--: | :- |
+ |     | (()) | (()) |    | 
+ |     |  No    |  Yes  |    | 
+
+---------------------------
+
+[//comment]: # "These are internal parameters for the experiment and visible texts not in this Markdown"
+[//comment]: # "----------"
+[//parameter]: # "ExperimentAcronym:Audiotest"
+[//parameter]: # "audioBaseURL:./Stimuli/"
+[//parameter]: # "PracticeItems:0"
+[//parameter]: # "ShuffleStimuli:true"
+[//parameter]: # "RandomizeAB:false"
+[//parameter]: # "body.style:"
+[//buttontext]: # "NextText:Next"
+[//buttontext]: # "NextAlertText:Please listen to the recordings and answer the questions first"
+[//buttontext]: # "ReadyText:Ready"
+[//buttontext]: # "PlayText:Play"
+[//buttontext]: # "RestartPageText:Restart"
+[//buttontext]: # "SaveButtonText:Save Results"
+[//buttontext]: # "SaveText:Please click XXSaveButtonTextXX and store the file"
+[//tooltiptext]: # "ToolTipPlay:Play sound"
+[//tooltiptext]: # "ToolTipNext:Go to next item"
+[//tooltiptext]: # "ToolTipReadyReady please save results"
+[//tooltiptext]: # "ToolTipRestart:Start a new experiment session"
+[//tooltiptext]: # "ToolTipSave:Save the answer to a file"
+[//comment]: # "----------"
+[//comment]: # "These are stimuli for this experiment"
+[//comment]: # "----------"
+[//stimulus0]: # "A"
+[//stimulus1]: # "https://upload.wikimedia.org/wikipedia/commons/6/62/De-Aluminium.ogg"
+[//stimulus1]: # "https://upload.wikimedia.org/wikipedia/commons/e/e7/Fr-bordure.ogg"
+[//comment]: # "----------"
+`
