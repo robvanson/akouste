04419a93722f2b709af122924ea99af79e65e9ba
Rob van Son <r.j.j.h.vanson@gmail.com>
2025-11-04 12:52:27 +0100

    Added full PseudoSpeech pseudonymization experiment

---
+++ ExpAcronym_examples/PseudoSpeech_markdown.js
@@ -0,0 +1,103 @@
+initialMarkdown = ` 
+<center>
+
+# Who is the unknown speaker X?
+
+## Speaker A or Speaker B?
+
+## {{{!answerstogo!|font-weight:bold}}} answers to go
+
+|     | [[[A]]] | [[[X]]] | [[[B]]] |     |
+| --- |:-------:|:-------:|:-------:| --- |
+
+</center>
+
+-----------------------
+
+<center>
+
+|     | (())  | Your Choice | (())  |     |
+| --- |:-----:|:-----------:|:-----:| --- |
+|     | **A** |             | **B** |     |
+
+</center>
+
+-----------------------------------
+
+[//comment]: # "These are internal parameters for the experiment and visible texts not in this Markdown"
+[//comment]: # "----------"
+[//parameter]: # "ExperimentAcronym:PseudoSpeech"
+[//parameter]: # "audioBaseURL:https://robvanson.github.io/PseudonymizeSpeech/Examples/Pseudonymized/"
+[//parameter]: # "PracticeItems:4"
+[//parameter]: # "ShuffleStimuli:true"
+[//parameter]: # "RandomizeAB:true"
+[//comment]: # "CSS style for HTML body of page"
+[//parameter]: # "body.style:"
+[//comment]: # "Add error checking digest to results"
+[//parameter]: # "addDigest:false"
+[//buttontext]: # "NextText:Next"
+[//buttontext]: # "NextAlertText:Please listen to the recordings and answer the questions first"
+[//buttontext]: # "ReadyText:Ready"
+[//buttontext]: # "PlayText:Play"
+[//buttontext]: # "RestartPageText:Restart"
+[//buttontext]: # "SaveButtonText:Save Results"
+[//buttontext]: # "SaveText:Please click XXSaveButtonTextXX and store the file"
+[//tooltiptext]: # "ToolTipPlay:Play sound"
+[//tooltiptext]: # "ToolTipNext:Go to next item"
+[//tooltiptext]: # "ToolTipReady:Ready&#44; please save results"
+[//tooltiptext]: # "ToolTipRestart:Start a new experiment session"
+[//tooltiptext]: # "ToolTipSave:Save the answer to a file"
+[//comment]: # "----------"
+[//comment]: # "These are stimuli for this experiment"
+[//comment]: # "----------"
+[//stimulus0]: # "A,B,X,SpA,SpB,SpX"
+[//stimulus1]: # "afo_chp27_utt063.wav,ksh_chp21_utt010.wav,afo_chp28_utt116.wav,afo,ksh,afo"
+[//stimulus1]: # "afo_chp28_utt158_500-120-4.0.wav,kmq_chp21_utt073_500-120-4.0.wav,afo_chp27_utt052.wav,afo,kmq,afo"
+[//stimulus1]: # "hmc_chp15_utt049_575-175-4.0.wav,afo_chp27_utt012_575-175-4.0.wav,afo_chp28_utt107.wav,hmc,afo,afo"
+[//stimulus1]: # "msm_chp33_utt042.wav,apr_chp27_utt003.wav,apr_chp20_utt126.wav,msm,apr,apr"
+[//stimulus1]: # "apr_chp07_utt013_500-120-4.0.wav,gma_chp26_utt020_500-120-4.0.wav,apr_chp10_utt043.wav,apr,gma,apr"
+[//stimulus1]: # "apr_chp04_utt011_575-175-4.0.wav,mjd_chp19_utt099_575-175-4.0.wav,apr_chp24_utt061.wav,apr,mjd,apr"
+[//stimulus1]: # "cve_chp18_utt054.wav,hmc_chp16_utt011.wav,cve_chp17_utt003.wav,cve,hmc,cve"
+[//stimulus1]: # "cve_chp17_utt011_500-120-4.0.wav,msm_chp16_utt048_500-120-4.0.wav,cve_chp17_utt083.wav,cve,msm,cve"
+[//stimulus1]: # "kmq_chp22_utt008_575-175-4.0.wav,cve_chp17_utt017_575-175-4.0.wav,cve_chp17_utt061.wav,kmq,cve,cve"
+[//stimulus1]: # "apr_chp11_utt143.wav,gma_chp26_utt019.wav,gma_chp03_utt016.wav,apr,gma,gma"
+[//stimulus1]: # "gma_chp04_utt020_500-120-4.0.wav,mbr_chp01_utt114_500-120-4.0.wav,gma_chp03_utt009.wav,gma,mbr,gma"
+[//stimulus1]: # "afo_chp27_utt023_575-175-4.0.wav,gma_chp03_utt066_575-175-4.0.wav,gma_chp04_utt071.wav,afo,gma,gma"
+[//stimulus1]: # "hmc_chp15_utt025.wav,gma_chp03_utt059.wav,hmc_chp15_utt062.wav,hmc,gma,hmc"
+[//stimulus1]: # "hmc_chp15_utt091_500-120-4.0.wav,nfo_chp24_utt053_500-120-4.0.wav,hmc_chp16_utt065.wav,hmc,nfo,hmc"
+[//stimulus1]: # "hmc_chp16_utt055_575-175-4.0.wav,ksh_chp03_utt103_575-175-4.0.wav,hmc_chp15_utt070.wav,hmc,ksh,hmc"
+[//stimulus1]: # "kmq_chp21_utt028.wav,iva_chp10_utt026.wav,iva_chp10_utt076.wav,kmq,iva,iva"
+[//stimulus1]: # "cve_chp17_utt023_500-120-4.0.wav,iva_chp10_utt049_500-120-4.0.wav,iva_chp09_utt076.wav,cve,iva,iva"
+[//stimulus1]: # "iva_chp09_utt055_575-175-4.0.wav,rph_chp33_utt108_575-175-4.0.wav,iva_chp10_utt055.wav,iva,rph,iva"
+[//stimulus1]: # "apr_chp27_utt014.wav,jsu_chp32_utt113.wav,jsu_chp31_utt053.wav,apr,jsu,jsu"
+[//stimulus1]: # "iva_chp09_utt105_500-120-4.0.wav,jsu_chp31_utt004_500-120-4.0.wav,jsu_chp32_utt081.wav,iva,jsu,jsu"
+[//stimulus1]: # "mdw_chp07_utt039_575-175-4.0.wav,jsu_chp32_utt064_575-175-4.0.wav,jsu_chp32_utt073.wav,mdw,jsu,jsu"
+[//stimulus1]: # "kmq_chp21_utt011.wav,hmc_chp15_utt113.wav,kmq_chp21_utt077.wav,kmq,hmc,kmq"
+[//stimulus1]: # "kmq_chp21_utt025_500-120-4.0.wav,mjd_chp19_utt051_500-120-4.0.wav,kmq_chp21_utt016.wav,kmq,mjd,kmq"
+[//stimulus1]: # "kmq_chp21_utt060_575-175-4.0.wav,jsu_chp31_utt063_575-175-4.0.wav,kmq_chp21_utt008.wav,kmq,jsu,kmq"
+[//stimulus1]: # "ksh_chp08_utt069.wav,msm_chp20_utt142.wav,ksh_chp18_utt010.wav,ksh,msm,ksh"
+[//stimulus1]: # "nfo_chp23_utt036_500-120-4.0.wav,ksh_chp31_utt009_500-120-4.0.wav,ksh_chp09_utt109.wav,nfo,ksh,ksh"
+[//stimulus1]: # "jsu_chp31_utt076_575-175-4.0.wav,ksh_chp05_utt063_575-175-4.0.wav,ksh_chp26_utt122.wav,jsu,ksh,ksh"
+[//stimulus1]: # "kmq_chp22_utt029.wav,ksh2_chp05_utt067.wav,ksh2_chp06_utt108.wav,kmq,ksh2,ksh2"
+[//stimulus1]: # "ksh2_chp06_utt054_500-120-4.0.wav,iva_chp09_utt127_500-120-4.0.wav,ksh2_chp06_utt031.wav,ksh2,iva,ksh2"
+[//stimulus1]: # "nfo_chp12_utt061_575-175-4.0.wav,ksh2_chp05_utt103_575-175-4.0.wav,ksh2_chp06_utt072.wav,nfo,ksh2,ksh2"
+[//stimulus1]: # "mdw_chp08_utt033.wav,mbr_chp01_utt107.wav,mbr_chp13_utt030.wav,mdw,mbr,mbr"
+[//stimulus1]: # "msm_chp19_utt079_500-120-4.0.wav,mbr_chp14_utt024_500-120-4.0.wav,mbr_chp13_utt063.wav,msm,mbr,mbr"
+[//stimulus1]: # "jsu_chp31_utt048_575-175-4.0.wav,mbr_chp13_utt066_575-175-4.0.wav,mbr_chp14_utt086.wav,jsu,mbr,mbr"
+[//stimulus1]: # "mdw_chp07_utt074.wav,nfo_chp23_utt012.wav,mdw_chp08_utt082.wav,mdw,nfo,mdw"
+[//stimulus1]: # "mdw_chp08_utt073_500-120-4.0.wav,iva_chp09_utt121_500-120-4.0.wav,mdw_chp07_utt018.wav,mdw,iva,mdw"
+[//stimulus1]: # "mdw_chp08_utt097_575-175-4.0.wav,ksh_chp26_utt060_575-175-4.0.wav,mdw_chp08_utt030.wav,mdw,ksh,mdw"
+[//stimulus1]: # "kmq_chp21_utt034.wav,mjd_chp20_utt119.wav,mjd_chp20_utt033.wav,kmq,mjd,mjd"
+[//stimulus1]: # "hmc_chp16_utt037_500-120-4.0.wav,mjd_chp20_utt030_500-120-4.0.wav,mjd_chp19_utt108.wav,hmc,mjd,mjd"
+[//stimulus1]: # "gma_chp26_utt131_575-175-4.0.wav,mjd_chp20_utt146_575-175-4.0.wav,mjd_chp20_utt122.wav,gma,mjd,mjd"
+[//stimulus1]: # "msm_chp20_utt066.wav,jsu_chp32_utt103.wav,msm_chp13_utt073.wav,msm,jsu,msm"
+[//stimulus1]: # "msm_chp20_utt049_500-120-4.0.wav,cve_chp17_utt045_500-120-4.0.wav,msm_chp31_utt055.wav,msm,cve,msm"
+[//stimulus1]: # "gma_chp03_utt079_575-175-4.0.wav,msm_chp25_utt007_575-175-4.0.wav,msm_chp31_utt093.wav,gma,msm,msm"
+[//stimulus1]: # "cve_chp17_utt043.wav,nfo_chp12_utt040.wav,nfo_chp11_utt108.wav,cve,nfo,nfo"
+[//stimulus1]: # "mbr_chp01_utt117_500-120-4.0.wav,nfo_chp12_utt110_500-120-4.0.wav,nfo_chp23_utt027.wav,mbr,nfo,nfo"
+[//stimulus1]: # "apr_chp10_utt087_575-175-4.0.wav,nfo_chp12_utt002_575-175-4.0.wav,nfo_chp11_utt092.wav,apr,nfo,nfo"
+[//stimulus1]: # "rph_chp30_utt101.wav,mjd_chp19_utt054.wav,rph_chp29_utt169.wav,rph,mjd,rph"
+[//stimulus1]: # "rph_chp29_utt061_500-120-4.0.wav,jsu_chp31_utt060_500-120-4.0.wav,rph_chp29_utt081.wav,rph,jsu,rph"
+[//stimulus1]: # "rph_chp33_utt117_575-175-4.0.wav,hmc_chp15_utt130_575-175-4.0.wav,rph_chp29_utt009.wav,rph,hmc,rph"
+[//comment]: # "----------"
+`
